23117c56fdfd62f33046a2ea5013d1c4b7b7159d
Juanse Gómez <js.gomez14@uniandes.edu.co>
2020-02-05 16:50:42 -0500

    Ls, pwd commands

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -9,6 +9,9 @@ trait Command {
 object Command {
 
   val MKDIR = "mkdir"
+  val LS = "ls"
+  val PWD = "pwd"
+
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
   }
@@ -23,6 +26,9 @@ object Command {
     else if(MKDIR.equals(tokens(0))){
       if(tokens.length < 2) incompleteCommand(MKDIR)
       else new Mkdir(tokens(1))
-    } else new UnknownCommand
+    }
+    else if(LS.equals(tokens(0))) new Ls
+    else if(PWD.equals(tokens(0))) new Pwd
+    else new UnknownCommand
   }
 }
